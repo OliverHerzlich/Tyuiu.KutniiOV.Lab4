e90f6fc5c201a55155740d9791b59d633f2152d4
Oliver <kutnii.o@gmail.com>
2025-01-17 11:09:46 +0500

    added Design forms

--- Tyuiu.KutniiOV.Lab4/Form1.Designer.cs
+++ Tyuiu.KutniiOV.Lab4/Form1.Designer.cs
@@ -28,18 +28,335 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button_7 = new Button();
+            button_8 = new Button();
+            button_9 = new Button();
+            button_4 = new Button();
+            button_5 = new Button();
+            button_6 = new Button();
+            button_1 = new Button();
+            button_2 = new Button();
+            button_3 = new Button();
+            button10 = new Button();
+            button_0 = new Button();
+            button_decimals = new Button();
+            button_del = new Button();
+            button_division = new Button();
+            button_plus = new Button();
+            button_minus = new Button();
+            button_root = new Button();
+            button_result = new Button();
+            button1 = new Button();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            toolStrip1 = new ToolStrip();
+            toolStripDropDownButton1 = new ToolStripDropDownButton();
+            toolStripMenuItem2 = new ToolStripMenuItem();
+            toolStripMenuItem3 = new ToolStripMenuItem();
+            toolStripMenuItem4 = new ToolStripMenuItem();
+            toolStripMenuItem5 = new ToolStripMenuItem();
+            toolStrip1.SuspendLayout();
             SuspendLayout();
+            // 
+            // button_7
+            // 
+            button_7.Location = new Point(12, 287);
+            button_7.Name = "button_7";
+            button_7.Size = new Size(118, 70);
+            button_7.TabIndex = 0;
+            button_7.Text = "7";
+            button_7.UseVisualStyleBackColor = true;
+            // 
+            // button_8
+            // 
+            button_8.Location = new Point(136, 287);
+            button_8.Name = "button_8";
+            button_8.Size = new Size(118, 70);
+            button_8.TabIndex = 1;
+            button_8.Text = "8";
+            button_8.UseVisualStyleBackColor = true;
+            // 
+            // button_9
+            // 
+            button_9.Location = new Point(260, 287);
+            button_9.Name = "button_9";
+            button_9.Size = new Size(118, 70);
+            button_9.TabIndex = 2;
+            button_9.Text = "9";
+            button_9.UseVisualStyleBackColor = true;
+            // 
+            // button_4
+            // 
+            button_4.Location = new Point(12, 363);
+            button_4.Name = "button_4";
+            button_4.Size = new Size(118, 70);
+            button_4.TabIndex = 3;
+            button_4.Text = "4";
+            button_4.UseVisualStyleBackColor = true;
+            // 
+            // button_5
+            // 
+            button_5.Location = new Point(136, 363);
+            button_5.Name = "button_5";
+            button_5.Size = new Size(118, 70);
+            button_5.TabIndex = 4;
+            button_5.Text = "6";
+            button_5.UseVisualStyleBackColor = true;
+            // 
+            // button_6
+            // 
+            button_6.Location = new Point(260, 363);
+            button_6.Name = "button_6";
+            button_6.Size = new Size(118, 70);
+            button_6.TabIndex = 5;
+            button_6.Text = "6";
+            button_6.UseVisualStyleBackColor = true;
+            // 
+            // button_1
+            // 
+            button_1.Location = new Point(12, 439);
+            button_1.Name = "button_1";
+            button_1.Size = new Size(118, 70);
+            button_1.TabIndex = 6;
+            button_1.Text = "1";
+            button_1.UseVisualStyleBackColor = true;
+            button_1.Click += button7_Click;
+            // 
+            // button_2
+            // 
+            button_2.Location = new Point(136, 439);
+            button_2.Name = "button_2";
+            button_2.Size = new Size(118, 70);
+            button_2.TabIndex = 7;
+            button_2.Text = "2";
+            button_2.UseVisualStyleBackColor = true;
+            // 
+            // button_3
+            // 
+            button_3.Location = new Point(260, 439);
+            button_3.Name = "button_3";
+            button_3.Size = new Size(118, 70);
+            button_3.TabIndex = 8;
+            button_3.Text = "3";
+            button_3.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(12, 515);
+            button10.Name = "button10";
+            button10.Size = new Size(118, 70);
+            button10.TabIndex = 10;
+            button10.Text = "+/-";
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button_0
+            // 
+            button_0.Location = new Point(136, 515);
+            button_0.Name = "button_0";
+            button_0.Size = new Size(118, 70);
+            button_0.TabIndex = 11;
+            button_0.Text = "0";
+            button_0.UseVisualStyleBackColor = true;
+            // 
+            // button_decimals
+            // 
+            button_decimals.Location = new Point(260, 515);
+            button_decimals.Name = "button_decimals";
+            button_decimals.Size = new Size(118, 70);
+            button_decimals.TabIndex = 12;
+            button_decimals.Text = ",";
+            button_decimals.UseVisualStyleBackColor = true;
+            // 
+            // button_del
+            // 
+            button_del.Location = new Point(384, 211);
+            button_del.Name = "button_del";
+            button_del.Size = new Size(95, 70);
+            button_del.TabIndex = 13;
+            button_del.Text = "⌫";
+            button_del.UseVisualStyleBackColor = true;
+            // 
+            // button_division
+            // 
+            button_division.Location = new Point(384, 287);
+            button_division.Name = "button_division";
+            button_division.Size = new Size(95, 70);
+            button_division.TabIndex = 14;
+            button_division.Text = "/";
+            button_division.UseVisualStyleBackColor = true;
+            // 
+            // button_plus
+            // 
+            button_plus.Location = new Point(384, 363);
+            button_plus.Name = "button_plus";
+            button_plus.Size = new Size(95, 70);
+            button_plus.TabIndex = 15;
+            button_plus.Text = "+";
+            button_plus.UseVisualStyleBackColor = true;
+            // 
+            // button_minus
+            // 
+            button_minus.Location = new Point(384, 439);
+            button_minus.Name = "button_minus";
+            button_minus.Size = new Size(95, 70);
+            button_minus.TabIndex = 16;
+            button_minus.Text = "-";
+            button_minus.UseVisualStyleBackColor = true;
+            // 
+            // button_root
+            // 
+            button_root.Location = new Point(260, 211);
+            button_root.Name = "button_root";
+            button_root.Size = new Size(118, 70);
+            button_root.TabIndex = 18;
+            button_root.Text = "√";
+            button_root.UseVisualStyleBackColor = true;
+            // 
+            // button_result
+            // 
+            button_result.Location = new Point(384, 515);
+            button_result.Name = "button_result";
+            button_result.Size = new Size(95, 70);
+            button_result.TabIndex = 19;
+            button_result.Text = "=";
+            button_result.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(136, 211);
+            button1.Name = "button1";
+            button1.Size = new Size(118, 70);
+            button1.TabIndex = 20;
+            button1.Text = "x^2";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(12, 211);
+            button2.Name = "button2";
+            button2.Size = new Size(118, 70);
+            button2.TabIndex = 21;
+            button2.Text = "x^n";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 32F);
+            textBox1.Location = new Point(12, 127);
+            textBox1.Name = "textBox1";
+            textBox1.RightToLeft = RightToLeft.Yes;
+            textBox1.Size = new Size(467, 78);
+            textBox1.TabIndex = 22;
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.ImageScalingSize = new Size(20, 20);
+            toolStrip1.Items.AddRange(new ToolStripItem[] { toolStripDropDownButton1 });
+            toolStrip1.Location = new Point(0, 0);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(491, 27);
+            toolStrip1.TabIndex = 23;
+            toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripDropDownButton1
+            // 
+            toolStripDropDownButton1.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripDropDownButton1.DropDownItems.AddRange(new ToolStripItem[] { toolStripMenuItem2, toolStripMenuItem3, toolStripMenuItem4, toolStripMenuItem5 });
+            toolStripDropDownButton1.Image = (Image)resources.GetObject("toolStripDropDownButton1.Image");
+            toolStripDropDownButton1.ImageTransparentColor = Color.Magenta;
+            toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            toolStripDropDownButton1.Size = new Size(34, 24);
+            toolStripDropDownButton1.Text = "toolStripDropDownButton1";
+            // 
+            // toolStripMenuItem2
+            // 
+            toolStripMenuItem2.Name = "toolStripMenuItem2";
+            toolStripMenuItem2.Size = new Size(224, 26);
+            toolStripMenuItem2.Text = "123";
+            // 
+            // toolStripMenuItem3
+            // 
+            toolStripMenuItem3.Name = "toolStripMenuItem3";
+            toolStripMenuItem3.Size = new Size(224, 26);
+            toolStripMenuItem3.Text = "123";
+            // 
+            // toolStripMenuItem4
+            // 
+            toolStripMenuItem4.Name = "toolStripMenuItem4";
+            toolStripMenuItem4.Size = new Size(224, 26);
+            toolStripMenuItem4.Text = "213";
+            // 
+            // toolStripMenuItem5
+            // 
+            toolStripMenuItem5.Name = "toolStripMenuItem5";
+            toolStripMenuItem5.Size = new Size(224, 26);
+            toolStripMenuItem5.Text = "123";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(491, 513);
+            ClientSize = new Size(491, 634);
+            Controls.Add(toolStrip1);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(button_result);
+            Controls.Add(button_root);
+            Controls.Add(button_minus);
+            Controls.Add(button_plus);
+            Controls.Add(button_division);
+            Controls.Add(button_del);
+            Controls.Add(button_decimals);
+            Controls.Add(button_0);
+            Controls.Add(button10);
+            Controls.Add(button_3);
+            Controls.Add(button_2);
+            Controls.Add(button_1);
+            Controls.Add(button_6);
+            Controls.Add(button_5);
+            Controls.Add(button_4);
+            Controls.Add(button_9);
+            Controls.Add(button_8);
+            Controls.Add(button_7);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button_7;
+        private Button button_8;
+        private Button button_9;
+        private Button button_4;
+        private Button button_5;
+        private Button button_6;
+        private Button button_1;
+        private Button button_2;
+        private Button button_3;
+        private Button button10;
+        private Button button_0;
+        private Button button_decimals;
+        private Button button_del;
+        private Button button_division;
+        private Button button_plus;
+        private Button button_minus;
+        private Button button_root;
+        private Button button_result;
+        private Button button1;
+        private Button button2;
+        private TextBox textBox1;
+        private ToolStrip toolStrip1;
+        private ToolStripDropDownButton toolStripDropDownButton1;
+        private ToolStripMenuItem toolStripMenuItem2;
+        private ToolStripMenuItem toolStripMenuItem3;
+        private ToolStripMenuItem toolStripMenuItem4;
+        private ToolStripMenuItem toolStripMenuItem5;
     }
 }
--- Tyuiu.KutniiOV.Lab4/Form1.cs
+++ Tyuiu.KutniiOV.Lab4/Form1.cs
@@ -6,5 +6,20 @@ namespace Tyuiu.KutniiOV.Lab4
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
